4fd338f6d691d8463e38399103663e8352c5f7ec
yuzuyuzu0830 <kiki.balon@gmail.com>
2021-01-29 19:03:15 +0900

    NewItemのfactory作成

---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\NewItem;
+use Faker\Generator as Faker;
+
+$factory->define(NewItem::class, function (Faker $faker) {
+    return [
+        'title' => $faker->realText($maxNbChars = 10, $indexSize = 5),
+        'user_id' => 1,
+        'color' => $faker->realText($maxNbChars = 10, $indexSize = 5),
+        'brand' => $faker->realText($maxNbChars = 10, $indexSize = 5),
+        'price' => $faker->numberBetween($min = 500, $max = 9000),
+        'start' => $faker->date($format = 'Y-m-d', $max = 'now'),
+        'main_category' => 'アイメイク',
+    ];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(StockCosmeticSeeder::class);
         $this->call(DoneTaskSeeder::class);
+        $this->call(NewItemSeeder::class);
     }
 }
