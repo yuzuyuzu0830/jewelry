56b196058114a0bab432e1a9de9c774e1d45341e
yuzuyuzu0830 <kiki.balon@gmail.com>
2021-02-01 23:24:12 +0900

    calendarを複数登録可能に変更

--- resources/views/layouts/modal.blade.php
+++ resources/views/layouts/modal.blade.php
@@ -7,17 +7,19 @@
                 <button class="modal__close" aria-label="Close modal" data-micromodal-close></button>
             </header>
             <main>
-
                 <!-- 入力フォーム -->
                 <form id="task-form" method="POST" action="{{ route('done_task') }}">
                     @csrf
                     <div class="tasks-group">
                         <p>できたことをチェック</p>
-                        <input id="brush" type="checkbox" name="title" value="ブラシ洗浄"><label for="brush">ブラシ洗浄</label>
-                        <input id="puff" type="checkbox" name="title" value="パフ洗浄"><label for="puff">パフ洗浄</label>
-                        <input id="pack" type="checkbox" name="title" value="顔のパック"><label for="pack">顔のパック</label><br>
-                        <input id="peeling" type="checkbox" name="title" value="ピーリング"><label for="peeling">ピーリング</label>
-                        <input id="treatment" type="checkbox" name="title" value="トリートメント"><label for="treatment">トリートメント</label>
+                        <input id="brush" type="checkbox" name="title[]" value="ブラシ洗浄"><label for="brush">ブラシ洗浄</label>
+                        <input id="puff" type="checkbox" name="title[]" value="パフ洗浄"><label for="puff">パフ洗浄</label>
+                        <input id="pack" type="checkbox" name="title[]" value="顔のパック"><label for="pack">顔のパック</label><br>
+                        <input id="peeling" type="checkbox" name="title[]" value="ピーリング"><label for="peeling">ピーリング</label>
+                        <input id="treatment" type="checkbox" name="title[]" value="トリートメント"><label for="treatment">トリートメント</label><br>
+                        <div class="form-check form-check-inline">
+                        <label for="other"><input type="checkbox"><label for="other">その他</label><input id="other" type="text" name="title[]">
+                        </div>
                     </div>
                     <div class="tasks-group">
                         <label>登録する日付</label><input type="date" class="task-form" name="start">
